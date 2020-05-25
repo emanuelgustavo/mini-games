c307f10502a01244d0f38c45acc5fa6111182a2a
Emanuel Gustavo Vierne <emanuelgustavovierne@gmail.com>
2020-05-25 12:56:11 -0300

    Update snake

--- snake-game/main.js
+++ snake-game/main.js
@@ -1,43 +1,118 @@
 window.onload = () => {
 	
+	const canvas = document.getElementById("canvas");
+	const context = canvas.getContext("2d");
+	
 	document.addEventListener('keydown', event => {
 		changeDirection(event);
 	});
 	
-	const getRandomPosition = ( min = 1, max = 32 ) => {
-		
+	const getRandomNumber = ( min = 1, max = 32 ) => {		
 		const position = Math.floor( Math.random() * (max - min) + min );
-		return position * boxSize;
-		
+		return position * boxSize;		
 	};
-	
-	const renderField = () => {		
-		context.fillStyle = "gray";
-		context.fillRect(0, 0, fieldSize, fieldSize);
-	};
-	
-	const canvas = document.getElementById("canvas");
-	const context = canvas.getContext("2d");
 	
 	//game variables
 	const fieldSize = 512;
 	const boxSize = 16;
 	let level = 1;
+	let speed = 1;
+	let time = 150;
 	
 	//snake variables
-	let posX = posY = getRandomPosition() / boxSize;
-	let speedX = 1;
+	let posX = posY = getRandomNumber() / boxSize;
+	let speedX = speed;
 	let speedY = 0;
-	let snakeLength = 1;
+	let snakeLength = 4;
 	let snake = []; 
 	
 	//food variables
-	let foodX = getRandomPosition();
-	let foodY = getRandomPosition();
+	const food = {
+		foodX: getRandomNumber(),
+		foodY: getRandomNumber(),
+		value: 1
+	};
 	
+	const bonusFood = {
+		foodX: getRandomNumber(),
+		foodY: getRandomNumber(),
+		value: getRandomNumber(1, 10),
+		time: 10
+	};
 	
-	setInterval( () => {
+	//render field
+	const renderField = () => {		
+		context.fillStyle = "gray";
+		context.fillRect(0, 0, fieldSize, fieldSize);
+	};
+	
+	//render snake
+	const renderSnake = () => {
 		
+		for ( let i = 0; i < snake.length; i++) {			
+			if ( posX === snake[i].posX && posY === snake[i].posY ) {				
+				alert(`Game Over. Pontuação: ${snakeLength}`);
+				snakeLength = 4;				
+				renderField();
+				posX = posY = getRandomNumber() / 16;				
+			}
+			i === snake.length - 1 ? context.fillStyle = "yellow" : context.fillStyle = "green";
+			context.fillRect( snake[i].posX * boxSize, snake[i].posY * boxSize, boxSize, boxSize);
+		};
+		
+		//add a tail in snake
+		snake.push({ 
+			posX, 
+			posY 
+		});
+		
+		//verify and mantain the snake length
+		while(snake.length > snakeLength) {
+			snake.shift();
+		};
+	}
+	
+	//render food in random place
+	const renderFood = () => {		
+		context.fillStyle = "red";
+		context.fillRect( food.foodX, food.foodY, boxSize, boxSize);
+	};
+	
+	//render bonus food
+	const renderBonusFood = () => {		
+		context.fillStyle = "blue";
+		context.fillRect( bonusFood.foodX, bonusFood.foodY, boxSize, boxSize);
+	};
+	
+	//change snake direction
+	const changeDirection = event => {
+		
+		switch(event.keyCode) {
+			case 38:
+				speedX = 0;
+				speedY = -speed;
+				break;
+			case 40:
+				speedX = 0;
+				speedY = speed;
+				break;
+			case 37:
+				speedX = -speed;
+				speedY = 0;
+				break;
+			case 39:
+				speedX = speed;
+				speedY = 0;
+				break;
+			default:
+				break;
+		};
+		
+	};
+	
+	const game = () => {
+		
+		//make snake runs
 		posX += speedX;
 		posY += speedY;
 		
@@ -55,91 +130,39 @@ window.onload = () => {
 			posY = fieldSize / boxSize;
 		}
 		
-		//console.log(`Posição atual cobra = (x, y) => (${posX}, ${posY})`);
-		//console.log(`Posição atual food = (x, y) => (${foodX}, ${foodY})`);
-		
 		//render field
 		renderField();
 		
-		//render snake
-		//context.fillStyle = "green";
-		//context.fillRect(posX * boxSize, posY * boxSize, boxSize, boxSize);
-		
-		//console.log(snake);
-		
-		for ( let i = 0; i < snake.length; i++) {
-			
-			if ( posX === snake[i].posX && posY === snake[i].posY ) {
-				
-				console.log(`Head position: (${posX}, ${posY}`); 
-				console.log(`Render position: (${snake[i].posX}, ${snake[i].posY})`);
-				
-				alert(`Game Over. Pontuação: ${snakeLength}`);
-				snakeLength = 10;
-				posX = posY = getRandomPosition() / 16;
-				renderField();
-			}
-			
-			i === snake.length - 1 ? context.fillStyle = "yellow" : context.fillStyle = "green";
-			context.fillRect( snake[i].posX * boxSize, snake[i].posY * boxSize, boxSize, boxSize );
-			
-			console.log(`Head position: (${posX}, ${posY}`); 
-			console.log(`Render position: (${snake[i].posX}, ${snake[i].posY})`);
-			
-		};
-		
-		snake.push({ 
-			posX, 
-			posY 
-		});
-		
-		while(snake.length > snakeLength) {
-			snake.shift();
-		};
+		//render snake		
+		renderSnake();
 				
 		//render food
-		context.fillStyle = "red";
-		context.fillRect( foodX, foodY, boxSize, boxSize);
+		renderFood();
+		
+		if ( snakeLength % 5 === 0 && bonusFood.time > 0 ) {
+			renderBonusFood();
+			bonusFood.time -= 1;
+			
+		}
 		
 		//when eat food
-		if ( posX * boxSize === foodX && posY * boxSize === foodY ) {
-			snakeLength += 1;
-			foodX = getRandomPosition();
-			foodY = getRandomPosition();
+		if ( posX * boxSize === food.foodX && posY * boxSize === food.foodY ) {
+			snakeLength += food.value;
+			food.foodX = getRandomNumber();
+			food.foodY = getRandomNumber();
 		};
 		
-		//change speed game
-		if ( snakeLength % 10 === 0 ) {
-			level = snakeLength / 10;
-		};
-		
-	console.log(`Snake length = ${snakeLength}`);
-		
-	}, 500 );
-	
-	const changeDirection = event => {
-		
-		switch(event.keyCode) {
-			case 38:
-				speedX = 0;
-				speedY = -1;
-				break;
-			case 40:
-				speedX = 0;
-				speedY = 1;
-				break;
-			case 37:
-				speedX = -1;
-				speedY = 0;
-				break;
-			case 39:
-				speedX = 1;
-				speedY = 0;
-				break;
-			default:
-				break;
+		//when eat bonus food
+		if ( posX * boxSize === bonusFood.foodX && posY * boxSize === bonusFood.foodY ) {
+			snakeLength += bonusFood.value;
+			bonusFood.foodX = getRandomNumber();
+			bonusFood.foodY = getRandomNumber();
 		};
 		
 	};
+	
+	setInterval( () => {		
+		game();		
+	}, time );
 	
 };
